ad96faa694aa98115f337f766beae1e08413c151
Vladislav Kalinovskij <vkalinovskij@gmail.com>
2023-03-08 01:15:10 +0000

    basic files and folders

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Banner } from "./components/Banner";
+import { Categories } from "./components/Categories";
+import { Game } from "./containers/Game";
+
+export const App = () => {
+    return (
+        <div>
+            <Banner />
+
+            <Categories />
+
+            <Game />
+
+
+
+
+        </div>);
+};
---
+++ src/containers/Game.js
@@ -0,0 +1,15 @@
+import { Keyboard } from "../components/Keyboard";
+import { Word } from "../components/Word";
+import { Strikes } from "../components/Strikes";
+
+export const Game = () => {
+    return (
+    <div>
+        <Word />
+
+        <Keyboard />
+
+        <Strikes />
+
+    </div>);
+};
